1f9ccb1a7bf42c799487910e5c99ccf63b028dbb
hiroto-ugajin <hrtugauga0324@gmail.com>
2023-05-31 11:11:02 +0900

    Lesson 7.7.9MainActivity完成

--- app/src/main/java/jp/techacademy/hiroto/ugajin/apiapp2/MainActivity.kt
+++ app/src/main/java/jp/techacademy/hiroto/ugajin/apiapp2/MainActivity.kt
@@ -2,11 +2,14 @@ package jp.techacademy.hiroto.ugajin.apiapp2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import jp.techacademy.hiroto.ugajin.apiapp2.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FragmentCallback {
     private lateinit var binding: ActivityMainBinding
 
     private val viewPagerAdapter by lazy { ViewPagerAdapter(this) }
@@ -30,5 +33,73 @@ class MainActivity : AppCompatActivity() {
         TabLayoutMediator(binding.tabLayout, binding.viewPager2) { tab, position ->
             tab.setText(viewPagerAdapter.titleIds[position])
         }.attach()
+
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            // タブが選択された際に呼ばれる
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                showFavoriteTabInfo(tab)
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+        })
+    }
+
+    /**
+     * お気に入りタブにトーストを表示
+     */
+    private fun showFavoriteTabInfo(tab: TabLayout.Tab) {
+        if (tab.position == VIEW_PAGER_POSITION_FAVORITE && FavoriteShop.findAll()
+                .isEmpty()
+        ) {
+            Toast.makeText(this@MainActivity, R.string.empty_favorite, Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+    /**
+     * Favoriteに追加するときのメソッド(Fragment -> Activity へ通知する)
+     */
+    override fun onAddFavorite(shop: Shop) {
+        FavoriteShop.insert(FavoriteShop().apply {
+            id = shop.id
+            name = shop.name
+            imageUrl = shop.logoImage
+            url = shop.couponUrls.sp.ifEmpty { shop.couponUrls.pc }
+        })
+        (viewPagerAdapter.fragments[VIEW_PAGER_POSITION_FAVORITE] as FavoriteFragment).updateData()
+    }
+
+    /**
+     * Favoriteから削除するときのメソッド(Fragment -> Activity へ通知する)
+     */
+    override fun onDeleteFavorite(id: String) {
+        showConfirmDeleteFavoriteDialog(id)
+    }
+
+    private fun showConfirmDeleteFavoriteDialog(id: String) {
+        AlertDialog.Builder(this)
+            .setTitle(R.string.delete_favorite_dialog_title)
+            .setMessage(R.string.delete_favorite_dialog_message)
+            .setPositiveButton(android.R.string.ok) { _, _ ->
+                deleteFavorite(id)
+                if (binding.tabLayout.selectedTabPosition == VIEW_PAGER_POSITION_FAVORITE) {
+                    showFavoriteTabInfo(binding.tabLayout.getTabAt(binding.tabLayout.selectedTabPosition)!!)
+                }
+            }
+            .setNegativeButton(android.R.string.cancel) { _, _ -> }
+            .create()
+            .show()
+    }
+
+    private fun deleteFavorite(id: String) {
+        FavoriteShop.delete(id)
+        (viewPagerAdapter.fragments[VIEW_PAGER_POSITION_API] as ApiFragment).updateView()
+        (viewPagerAdapter.fragments[VIEW_PAGER_POSITION_FAVORITE] as FavoriteFragment).updateData()
+    }
+
+    companion object {
+        private const val VIEW_PAGER_POSITION_API = 0
+        private const val VIEW_PAGER_POSITION_FAVORITE = 1
     }
 }
